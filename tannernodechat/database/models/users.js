abfdc9fd2c97c3bce8a2e566467e9be7b33571e8
tannerwick <117384118+tannerwick@users.noreply.github.com>
2022-11-03 12:58:06 -0400

    Delete tannernodechat directory

--- tannernodechat/database/models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-let users = new mongoose.Schema({
-  username: String,
-  password: String,
-  admin: Number
-});
-
-module.exports = mongoose.model("users", users);
